a427032bc9a0e814bf85b2fbf01ec0ee36c5b6e3
luizfernandomodesto <LFMronan45@gmail.com>
2026-05-16 20:20:13 -0300

    feat: Implementar a classe System nos arquivos mySim.h e mySim.cpp

--- src/mySim.cpp
+++ src/mySim.cpp
@@ -2,7 +2,47 @@
 #include<iostream>
 #include<algorithm>
 
+//Classe System
+System::System() {
+    this->name = "";
+    this->value = 0.0;
+}
 
+System::System(std::string name, double value) : name(name), value(value) {}
+
+System::~System() {}
+
+void System::setName(std::string name) {
+    this->name = name;
+}
+
+std::string System::getName() const {
+    return this->name;
+}
+
+void System::setValue(double value) {
+    this->value = value;
+}
+
+double System::getValue() const {
+    return this->value;
+}
+
+//construtor de copia
+System::System(const System& sys) {
+    this->name = sys.name;
+    this->value = sys.value;
+}
+
+//Atribuição por =
+System& System::operator=(const System& sys) {
+    if (&sys == this) {
+        return *this; //são iguais
+    }
+    this->name = sys.name;
+    this->value = sys.value;
+    return *this;
+}
 
 //Classe Fluxo
 Flow::Flow(){
