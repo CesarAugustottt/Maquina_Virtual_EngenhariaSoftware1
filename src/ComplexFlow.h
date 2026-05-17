a8e99cef4ea607efac449e7ca9b902963ea2f13f
luizfernandomodesto <LFMronan45@gmail.com>
2026-05-17 01:20:59 -0300

    feat: implementa o cabeçalho da classe ComplexlFlow

---
+++ src/ComplexFlow.h
@@ -0,0 +1,24 @@
+#ifndef COMPLEXFLOW_H
+#define COMPLEXFLOW_H
+
+#include "Flow.h"
+
+class ComplexFlow : public Flow {
+public:
+    // Construtor padrao
+    ComplexFlow();
+    // Construtor parametrizado
+    ComplexFlow(std::string name, System* source, System* target);
+    // Destrutor virtual
+    virtual ~ComplexFlow();
+
+    // Sobrescrita do execute
+    virtual double execute() override;
+
+    // Construtor de copia
+    ComplexFlow(const ComplexFlow& flow);
+    // Operador de atribuicao
+    ComplexFlow& operator=(const ComplexFlow& flow);
+};
+
+#endif
