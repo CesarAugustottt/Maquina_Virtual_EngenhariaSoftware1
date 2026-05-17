3c5adbe9ff204df4b989f1c01fe3f30b1008bbb5
luizfernandomodesto <LFMronan45@gmail.com>
2026-05-16 23:36:42 -0300

    feat: implementa o cabeçalho da classe ExponentialFlow

---
+++ src/ExponentialFlow.h
@@ -0,0 +1,22 @@
+#ifndef EXPONENTIALFLOW_H
+#define EXPONENTIALFLOW_H
+
+#include "Flow.h"
+
+class ExponentialFlow : public Flow {
+public:
+    // Construtor padrao
+    ExponentialFlow();
+    // Construtor parametrizado
+    ExponentialFlow(std::string name, System* source, System* target);
+    // Destrutor virtual
+    virtual ~ExponentialFlow();
+    // Sobrescrita do execute
+    virtual double execute() override;
+    // Construtor de copia
+    ExponentialFlow(const ExponentialFlow& flow);
+    // Operador de atribuicao
+    ExponentialFlow& operator=(const ExponentialFlow& flow);
+};
+
+#endif
