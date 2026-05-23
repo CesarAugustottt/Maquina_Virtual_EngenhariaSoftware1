7eeb78d8f88e2b87fa4d9c9504320c5c1c0b247e
luizfernandomodesto <LFMronan45@gmail.com>
2026-05-23 20:55:16 -0300

    docs: padroniza tags doxygen em ingles na classe ExponentialFlow

--- src/ExponentialFlow.h
+++ src/ExponentialFlow.h
@@ -3,19 +3,48 @@
 
 #include "Flow.h"
 
+/*!
+ * @brief This Class represents an exponential flow in the General Systems Theory implemented in this code.
+ */
 class ExponentialFlow : public Flow {
 public:
     // Construtor padrao
+    /*!
+     * @brief This is the default constructor for the ExponentialFlow Class.
+     * * @return ExponentialFlow - an ExponentialFlow Class object.
+     */
     ExponentialFlow();
     // Construtor parametrizado
+    /*!
+     * @brief This is the parameterized constructor for the ExponentialFlow Class.
+     * * @param name the name of the ExponentialFlow.
+     * @param source a pointer to the source System.
+     * @param target a pointer to the target System.
+     */
     ExponentialFlow(std::string name, System* source, System* target);
     // Destrutor virtual
+    /*!
+     * @brief This is the default destructor for the ExponentialFlow Class.
+     */
     virtual ~ExponentialFlow();
     // Sobrescrita do execute
+    /*!
+     * @brief Executes the exponential equation to calculate the flow's value.
+     * * @return double - the calculated value for the transfer.
+     */
     virtual double execute() override;
     // Construtor de copia
+    /*!
+     * @brief This is the copy constructor for the ExponentialFlow Class.
+     * * @param flow the ExponentialFlow that is going to be cloned.
+     */
     ExponentialFlow(const ExponentialFlow& flow);
     // Operador de atribuicao
+    /*!
+     * @brief This is the overloaded assignment operator for the ExponentialFlow Class.
+     * * @param flow the ExponentialFlow that is going to be cloned.
+     * @return ExponentialFlow& - a reference to the updated ExponentialFlow Class object.
+     */
     ExponentialFlow& operator=(const ExponentialFlow& flow);
 };
 
