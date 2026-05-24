0540a55ab36c9ca32ff577058ca2542bf134685b
luizfernandomodesto <LFMronan45@gmail.com>
2026-05-23 22:43:28 -0300

    docs: padroniza tags doxygen em ingles

--- test/funcional/funcional_tests.h
+++ test/funcional/funcional_tests.h
@@ -11,11 +11,27 @@
 #include<cassert> //para a função assert
 #include<cmath> //Para a função fabs()
 
+//! Functional tests header
+/**
+ * Prototypes for the funcional tests of the simulation framework.
+ * This file declares the test cases responsible for validating the dynamics 
+ * between the Model, System, and Flow.
+ */
+
 //assinatura das funções:
+/*!
+  Function prototype for the exponential flow functional test.
+*/
 void exponentialFuncionalTest();
 
+/*!
+  Function prototype for the logistical flow functional test.
+*/
 void logisticalFuncionalTest();
 
+/*!
+  Function prototype for the complex flows functional tests.
+*/
 void complexFuncionalTest();
 
 #endif
