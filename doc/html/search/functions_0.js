6664116253f6dbb773d314ab071e69a832fc6def
Cesar Augusto <cesar.toto@aluno.ufop.edu.br>
2026-05-30 18:24:16 -0300

    doc: Atualização da documentação

--- doc/html/search/functions_0.js
+++ doc/html/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['add_94',['add',['../classModel.html#a94c8319fa87ab9f33a4ac2b362837e5d',1,'Model::add(System *sys)=0'],['../classModel.html#ab7df81041f62f049cfc6e4d50dcaf721',1,'Model::add(Flow *flow)=0'],['../classModelImpl.html#a4f2039580d1464b29593df95f3979998',1,'ModelImpl::add(System *sys) override'],['../classModelImpl.html#a3e8843e5353f40ab30afcf19b4f13a67',1,'ModelImpl::add(Flow *flow) override']]]
+  ['add_94',['add',['../classModel.html#a70362afdd9db6268b4adaedfeddb2935',1,'Model::add(System *)=0'],['../classModel.html#ac1e19f5fcc4ced9defbfe2847a26ff33',1,'Model::add(Flow *)=0'],['../classModelImpl.html#a4f2039580d1464b29593df95f3979998',1,'ModelImpl::add(System *sys) override'],['../classModelImpl.html#a3e8843e5353f40ab30afcf19b4f13a67',1,'ModelImpl::add(Flow *flow) override']]]
 ];
--- doc/html/search/functions_5.js
+++ doc/html/search/functions_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['incrementtime_109',['incrementTime',['../classModel.html#adc78826b46538af6cc18fb41c138a017',1,'Model::incrementTime()'],['../classModelImpl.html#a2b96592e72a176881c9ac01b85dc9158',1,'ModelImpl::incrementTime()']]]
+  ['incrementtime_109',['incrementTime',['../classModel.html#ad8f1c8d076bc82d40636a1ca0e581b92',1,'Model::incrementTime()'],['../classModelImpl.html#a2b96592e72a176881c9ac01b85dc9158',1,'ModelImpl::incrementTime()']]]
 ];
