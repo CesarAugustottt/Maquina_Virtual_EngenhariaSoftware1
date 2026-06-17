4387b8cf6fdfa39296a3aa2340cee395cd47ba60
luizfernandomodesto <LFMronan45@gmail.com>
2026-06-17 19:20:29 -0300

    refactor: atualiza a herança de flow em ComplexFlow.h

--- test/funcional/ComplexFlow.h
+++ test/funcional/ComplexFlow.h
@@ -2,6 +2,8 @@
 #define COMPLEXFLOW_H
 
 #include "../../src/Flow.h"
+#include "../../src/System.h"
+#include <string>
 
 /*!
  * @brief This Class represents a complex flow in the General Systems Theory implemented in this code.
@@ -15,13 +17,13 @@ protected:
     /*! This pointer points to the target System of the flow. */
     System* target;
 
-    // Construtor padrao
+public:
     /*!
      * @brief This is the default constructor for the ComplexFlow Class.
      * @return ComplexFlow - a ComplexFlow Class object.
      */
     ComplexFlow();
-    // Construtor parametrizado
+    
     /*!
      * @brief This is the parameterized constructor for the ComplexFlow Class.
      * @param name the name of the ComplexFlow.
@@ -30,43 +32,23 @@ protected:
      */
     ComplexFlow(std::string name, System* source, System* target);
 
-    // Construtor de copia
-    /*!
-     * @brief This is the copy constructor for the ComplexFlow Class.
-     * @param flow the ComplexFlow that is going to be cloned.
-     */
-    ComplexFlow(const ComplexFlow& flow);
-    // Operador de atribuicao
-    /*!
-     * @brief This is the overloaded assignment operator for the ComplexFlow Class.
-     * @param flow the ComplexFlow that is going to be cloned.
-     * @return ComplexFlow& - a reference to the updated ComplexFlow Class object.
-     */
-    ComplexFlow& operator=(const ComplexFlow& flow);
-public:
-    // Destrutor virtual
     /*!
      * @brief This is the default destructor for the ComplexFlow Class.
      */
     virtual ~ComplexFlow();
 
-    // Sobrescrita do metodo execute
     /*!
      * @brief Executes the complex equation to calculate the flow's value.
      * @return double - the calculated value for the transfer.
      */
     virtual double execute() override;
 
-    //getters e setters
     void setName(std::string name) override;
-    std::string getName()const override;
+    std::string getName() const override;
     void setSource(System* source) override;
-    System* getSource()const override;
+    System* getSource() const override;
     void setTarget(System* target) override;
-    System* getTarget()const override;
-
-    //permite que o template createFloe de Model instacie os fluxos
-    friend class Model;
+    System* getTarget() const override;
 };
 
 #endif
