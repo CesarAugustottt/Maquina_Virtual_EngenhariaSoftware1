a1dace8d16b015855d7a24763780689019228673
Cesar Augusto <cesar.toto@aluno.ufop.edu.br>
2026-06-24 15:59:05 -0300

    Teste funcional para testar Sistemas Compostos.

---
+++ test/funcional/funcional_compositeSystem_tests.h
@@ -0,0 +1,9 @@
+#ifndef FUNCIONAL_COMPOSITESYSTEM_TESTS_H
+#define FUNCIONAL_COMPOSITESYSTEM_TESTS_H
+
+/*!
+  Function prototype for the Composite System functional tests.
+*/
+void compositeSystemFuncionalTest();
+
+#endif
