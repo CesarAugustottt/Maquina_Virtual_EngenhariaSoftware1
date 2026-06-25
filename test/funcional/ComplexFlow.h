f6969c0d87488dd2f7edef3c59ecbfbc86b92b75
luizfernandomodesto <LFMronan45@gmail.com>
2026-06-25 12:29:11 -0300

    fix: herda de FlowHandle

--- test/funcional/ComplexFlow.h
+++ test/funcional/ComplexFlow.h
@@ -1,26 +1,17 @@
 #ifndef COMPLEXFLOW_H
 #define COMPLEXFLOW_H
 
-#include "../../src/Flow.h"
+#include "../../src/FlowImpl.h"
 #include "../../src/System.h"
 #include <string>
 
 /*!
  * @brief This Class represents a complex flow in the General Systems Theory implemented in this code.
  */
-class ComplexFlow : public Flow {
-protected:
-    /*! This attribute contains a name for the flow. */
-    std::string name;
-    /*! This pointer points to the source System of the flow. */
-    System* source;
-    /*! This pointer points to the target System of the flow. */
-    System* target;
-
+class ComplexFlow : public FlowHandle {
 public:
     /*!
      * @brief This is the default constructor for the ComplexFlow Class.
-     * @return ComplexFlow - a ComplexFlow Class object.
      */
     ComplexFlow();
     
@@ -39,16 +30,8 @@ public:
 
     /*!
      * @brief Executes the complex equation to calculate the flow's value.
-     * @return double - the calculated value for the transfer.
      */
     virtual double execute() override;
-
-    void setName(std::string name) override;
-    std::string getName() const override;
-    void setSource(System* source) override;
-    System* getSource() const override;
-    void setTarget(System* target) override;
-    System* getTarget() const override;
 };
 
 #endif
