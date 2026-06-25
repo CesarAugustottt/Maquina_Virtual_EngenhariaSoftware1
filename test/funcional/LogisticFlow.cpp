fa4d06265c5d4134a759d2b39fe318c00073adba
luizfernandomodesto <LFMronan45@gmail.com>
2026-06-25 12:27:01 -0300

    fix: herda de FlowHandle

--- test/funcional/LogisticFlow.cpp
+++ test/funcional/LogisticFlow.cpp
@@ -1,47 +1,16 @@
 #include "LogisticFlow.h"
 
-// Construtor padrao
-LogisticFlow::LogisticFlow() {
-    this->name = "";
-    this->source = nullptr;
-    this->target = nullptr;
-}
+LogisticFlow::LogisticFlow() : FlowHandle() {}
 
-// Construtor parametrizado
 LogisticFlow::LogisticFlow(std::string name, System* source, System* target)
-    : name(name), source(source), target(target) {}
+    : FlowHandle(name, source, target) {}
 
 LogisticFlow::~LogisticFlow() {} 
 
-void LogisticFlow::setName(std::string name) {
-    this->name = name;
-}
-
-std::string LogisticFlow::getName() const {
-    return this->name;
-}
-
-void LogisticFlow::setSource(System* source) {
-    this->source = source;
-}
-
-System* LogisticFlow::getSource() const {
-    return this->source;
-}
-
-void LogisticFlow::setTarget(System* target) {
-    this->target = target;
-}
-
-System* LogisticFlow::getTarget() const {
-    return this->target;
-}
-
-// Implementacao do calculo
 double LogisticFlow::execute() {
-    if(this->target == nullptr) { // Proteção contra ponteiro nulo
+    if (this->getTarget() == nullptr) {
         return 0.0;
     }
-    double valueTarget = this->target->getValue();
+    double valueTarget = this->getTarget()->getValue();
     return 0.01 * valueTarget * (1 - valueTarget / 70.0);
 }
